86aac3ea94711d26e82a0fbb959eebc0e230a057
alnraj2201 <alanraj.c@triassicsolutions.com>
2021-10-17 12:36:10 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # rabbitmq
 A simple RabbitMQ python script
+
+import json
+import time
+import QueueManager
+
+
+class SampleQueueWriteManager:
+
+    def __init__(self):
+        self.queue_manager = QueueManager()
+
+
+    def process_write_data(self):
+		"""
+		======================================================================
+		* Method Name : process_write_data
+		* Description : Read the JSON data and send it to the queue
+		* Args   : None
+		* Return : None
+		======================================================================
+		"""
+        while True:
+
+            with open('D:\Projects\sample_json.json', 'r') as file:
+                json_dict = json.load(file)
+
+            point_list = {}
+			
+			for data in json_dict:
+			    point_list['uid'] = data['ID']
+                point_list['name'] = data['Name']
+                self.add_to_queue(point_list)
+
+            time.sleep(5)
+
+
+    def add_to_queue(self, json_point_list):
+		"""
+		======================================================================
+		* Method Name : add_to_queue
+		* Description : Add the data to queue
+		* Args   : json_point_list
+		* Return : None
+		======================================================================
+		"""
+        queue_name = json_point_list['device_name']
+
+		self.queue_manager.create_queue(queue_name)
+		self.queue_manager.add_data_to_queue(queue_name, json_point_list)
---
+++ queue_manager.py
@@ -0,0 +1,89 @@
+"""
+======================================================================
+* Module Name : queue_manager.py
+* Created by  : Alan Raj C
+* Created on  : 30-Sep-2020
+* Description : This file manages the class handling Queues in RabbitMQ
+======================================================================
+"""
+
+import pika
+
+
+class QueueManager:
+    """
+    ======================================================================
+    * Class Name  : QueueManager
+    * Description : Contain all the methods in/for handling a queue
+    ======================================================================
+    """
+
+    def __init__(self):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+        self.channel = self.connection.channel()
+
+
+    def create_rabbitmq_connection(self):
+        """
+        ======================================================================
+        * Method Name : create_rabbitmq_connection
+        * Description : Creates the connection with the RabbitMQ server
+        * Args   : None
+        * Return : None
+        ======================================================================
+        """
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+        self.channel = self.connection.channel()
+
+
+    def add_data_to_queue(self, queue_name, json_point_list):
+        """
+        ======================================================================
+        * Method Name : add_data_to_queue
+        * Description : Add data to the queue created
+        * Args   : queue_name
+                 : json_point_list -> Body of the queue
+        * Return : None
+        ======================================================================
+        """
+        point_list = str(json_point_list)
+        self.channel.basic_publish(exchange='', routing_key=queue_name, body=point_list)
+
+
+    def create_queue(self, queue_name):
+        """
+        ======================================================================
+        * Method Name : create_queue
+        * Description : Create a queue with the given queue name.
+        * Args   : queue_name
+        * Return : None
+        ======================================================================
+        """
+        self.channel.queue_declare(queue=queue_name)
+
+
+    def consume_queues(self, queue_name, callback):
+        """
+        ======================================================================
+        * Method Name : consume_queues
+        * Description : Consume/subscribe to the given queue
+        * Args   : queue_name
+                 : callback       -> The callback method
+        * Return : None
+        ======================================================================
+        """
+        self.channel.basic_consume(queue=queue_name, on_message_callback=callback)
+
+
+    def consume_start(self):
+        """
+        ======================================================================
+        * Method Name : consume_start
+        * Description : Start the Consume/subscribe process
+        * Args   : None
+        * Return : None
+        ======================================================================
+        """
+        self.channel.start_consuming()
